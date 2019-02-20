5a195b1e3e812c87da073b8e4201b42ae46e2a4b
Aditya <aditya.dew2597@gmail.com>
2019-02-20 16:11:28 +0700

    add title in dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,45 +1,48 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="row">
-  @foreach($total as $t)
-  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
-    <div class="card card-statistics">
-      <div class="card-body">
-        <div class="clearfix">
-          <div class="float-left">
-            <i class="mdi mdi-account icon-lg"></i>
-          </div>
-          <div class="float-right">
-            <p class="mb-0 text-right">{{$t->name}}</p>
-            <div class="fluid-container">
-              <h3 class="font-weight-medium text-right mb-0">{{$t->jumlah}} Orang</h3>
+<fieldset>
+    <legend>Jumlah Pasien Berdasarkan Jenjang</legend>
+    <div class="row">
+      @foreach($total as $t)
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+        <div class="card card-statistics">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi mdi-account icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">{{$t->name}}</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">{{$t->jumlah}} Orang</h3>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-  @endforeach
+      @endforeach
 
-  @if($other > 0)
-  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
-    <div class="card card-statistics">
-      <div class="card-body">
-        <div class="clearfix">
-          <div class="float-left">
-            <i class="mdi mdi-account text-danger icon-lg"></i>
-          </div>
-          <div class="float-right">
-            <p class="mb-0 text-right">Lainnya</p>
-            <div class="fluid-container">
-              <h3 class="font-weight-medium text-right mb-0">{{$other}} Orang</h3>
+      @if($other > 0)
+      <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+        <div class="card card-statistics">
+          <div class="card-body">
+            <div class="clearfix">
+              <div class="float-left">
+                <i class="mdi mdi-account text-danger icon-lg"></i>
+              </div>
+              <div class="float-right">
+                <p class="mb-0 text-right">Lainnya</p>
+                <div class="fluid-container">
+                  <h3 class="font-weight-medium text-right mb-0">{{$other}} Orang</h3>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      @endif
     </div>
-  </div>
-  @endif
-</div>
+</fieldset>
 @endsection
